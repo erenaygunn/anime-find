fa0344a820b314da41223e47363fd5d70cbc01b1
erenaygunn <eaygun178@gmail.com>
2023-07-27 16:39:23 +0300

    searchbar done

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,7 +5,7 @@ import search from "../../images/1.png";
 export default function SearchBar() {
   return (
     <div className="search">
-      <input type="text" className="search-input" placeholder="Search for series..."/>
+      <input type="text" className="search-input" placeholder="Search for animes..."/>
       <button className="search-button"><img src={search} alt="" /></button>
     </div>
   );

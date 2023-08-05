6a2fbd845f32bb4f43e7d9e0c0758410b95d6cdd
erenaygunn <eaygun178@gmail.com>
2023-08-06 02:26:20 +0300

    navbar higlight active page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,26 @@ import "./navbar.css";
 import Searchbar from "../SearchBar/SearchBar"
 
 const Navbar = () => {
+
+  
+  const navLinks = document.querySelectorAll('.nav-links li a');
+
+  navLinks.forEach(link => {
+    if((link.getAttribute('href')) === window.location.pathname){
+      link.classList.add('active')
+    }
+  });
+
   return (
     <nav className='navbar'>
       <a href="/">AnimeFind</a>
       <Searchbar></Searchbar>
-      <div className='navlinks'>
-        <a href='/airing'>AIRING</a>
-        <a href='/upcoming'>UPCOMING</a>
-        <a href='/upcoming'>GENRES</a>
-      </div>
+      <ul className='nav-links'>
+        <li><a href='/popular'>POPULAR</a></li>
+        <li><a href='/airing'>AIRING</a></li>
+        <li><a href='/upcoming'>UPCOMING</a></li>
+        <li><a href='/genres'>GENRES</a></li>
+      </ul>
     </nav>
   )
 }
--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -11,7 +11,6 @@ const Popular = () => {
   const conditionalRender = () => {
     if(!isSearch){
       return popularAnime.map((anime) => {
-        console.log(anime)
         return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
           <img src={anime.images.jpg.large_image_url} alt="" />
         </Link> 
